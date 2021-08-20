9fdb54e8fe5c826ee2569165a591a2da0a3c794a
alena-yudzina <lenok.ok.lol@gmail.com>
2021-08-20 15:15:57 +0300

    add db_index to new_buildings

--- property/models.py
+++ property/models.py
@@ -10,7 +10,7 @@ class Flat(models.Model):
         default=timezone.now,
         db_index=True)
 
-    new_building = models.NullBooleanField('Новостройка', default=None)
+    new_building = models.NullBooleanField('Новостройка', default=None, db_index=True)
 
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
